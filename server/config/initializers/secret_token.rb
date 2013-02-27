06045b46158b674b313613a7fceb69cf6af5f199
twer <twer@localhost.(none)>
2013-02-27 10:40:21 +0800

    jtong - add server

---
+++ server/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Server::Application.config.secret_token = 'c5e3dc5c6b1be2c0d9da5e3c178e21cb2b46df6ebc448a97ede58aa255272f90aceda44d24774d7008eded0a5bee50b5716a704e31f1759b479cfea9d5fe415d'
