28da44eee7ec63e492cb5457ad652044b71de969
twer <twer@localhost.(none)>
2013-04-05 23:57:39 +0800

    jtong - import new template

--- server/app/controllers/dev_exam_controller.rb
+++ server/app/controllers/dev_exam_controller.rb
@@ -1,10 +1,12 @@
 class DevExamController < ApplicationController
   def kata
+    @current_steps = 2
     @katas = Kata.all
     @my_posted_kata = MyPostedKata.find_by_user_id current_user.id
   end
 
   def choose_a_kata
+    @current_steps = 2
     @kata = Kata.find params[:id]
     @my_posted_kata = MyPostedKata.new ({
       kata: @kata,
@@ -16,7 +18,11 @@ class DevExamController < ApplicationController
   end
 
   def homework
+    @current_steps = 3
+  end
 
+  def wizard
+    @current_steps = 1
   end
 
 end
