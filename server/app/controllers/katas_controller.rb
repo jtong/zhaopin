bdd1ef60cca80b94338f7daca3dffdc8a3609ad2
twer <twer@localhost.(none)>
2013-04-05 15:22:32 +0800

    jtong - fix view spell issue

--- server/app/controllers/katas_controller.rb
+++ server/app/controllers/katas_controller.rb
@@ -2,53 +2,53 @@ class KatasController < ApplicationController
   # GET /kata
   # GET /kata.json
   def index
-    @kata = Katum.all
+    @katas = Kata.all
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @kata }
+      format.json { render json: @katas }
     end
   end
 
   # GET /kata/1
   # GET /kata/1.json
   def show
-    @katum = Katum.find(params[:id])
+    @kata = Kata.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @katum }
+      format.json { render json: @kata }
     end
   end
 
   # GET /kata/new
   # GET /kata/new.json
   def new
-    @katum = Katum.new
+    @kata = Kata.new
 
     respond_to do |format|
       format.html # new.html.erb
-      format.json { render json: @katum }
+      format.json { render json: @kata }
     end
   end
 
   # GET /kata/1/edit
   def edit
-    @katum = Katum.find(params[:id])
+    @kata = Kata.find(params[:id])
   end
 
   # POST /kata
   # POST /kata.json
   def create
-    @katum = Katum.new(params[:katum])
+    @kata = Kata.new(params[:kata])
 
     respond_to do |format|
-      if @katum.save
-        format.html { redirect_to @katum, notice: 'Katum was successfully created.' }
-        format.json { render json: @katum, status: :created, location: @katum }
+      if @kata.save
+        format.html { redirect_to @kata, notice: 'Katum was successfully created.' }
+        format.json { render json: @kata, status: :created, location: @kata }
       else
         format.html { render action: "new" }
-        format.json { render json: @katum.errors, status: :unprocessable_entity }
+        format.json { render json: @kata.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -56,15 +56,15 @@ class KatasController < ApplicationController
   # PUT /kata/1
   # PUT /kata/1.json
   def update
-    @katum = Katum.find(params[:id])
+    @kata = Kata.find(params[:id])
 
     respond_to do |format|
-      if @katum.update_attributes(params[:katum])
-        format.html { redirect_to @katum, notice: 'Katum was successfully updated.' }
+      if @kata.update_attributes(params[:kata])
+        format.html { redirect_to @kata, notice: 'Katum was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
-        format.json { render json: @katum.errors, status: :unprocessable_entity }
+        format.json { render json: @kata.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -72,8 +72,8 @@ class KatasController < ApplicationController
   # DELETE /kata/1
   # DELETE /kata/1.json
   def destroy
-    @katum = Katum.find(params[:id])
-    @katum.destroy
+    @kata = Kata.find(params[:id])
+    @kata.destroy
 
     respond_to do |format|
       format.html { redirect_to kata_url }
--- server/app/controllers/my_posted_katas_controller.rb
+++ server/app/controllers/my_posted_katas_controller.rb
@@ -2,7 +2,7 @@ class MyPostedKatasController < ApplicationController
   # GET /my_posted_kata
   # GET /my_posted_kata.json
   def index
-    @my_posted_kata = MyPostedKatum.all
+    @my_posted_kata = MyPostedKata.all
 
     respond_to do |format|
       format.html # index.html.erb
@@ -13,42 +13,42 @@ class MyPostedKatasController < ApplicationController
   # GET /my_posted_kata/1
   # GET /my_posted_kata/1.json
   def show
-    @my_posted_katum = MyPostedKatum.find(params[:id])
+    @my_posted_kata = MyPostedKata.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @my_posted_katum }
+      format.json { render json: @my_posted_kata }
     end
   end
 
   # GET /my_posted_kata/new
   # GET /my_posted_kata/new.json
   def new
-    @my_posted_katum = MyPostedKatum.new
+    @my_posted_kata = MyPostedKata.new
 
     respond_to do |format|
       format.html # new.html.erb
-      format.json { render json: @my_posted_katum }
+      format.json { render json: @my_posted_kata }
     end
   end
 
   # GET /my_posted_kata/1/edit
   def edit
-    @my_posted_katum = MyPostedKatum.find(params[:id])
+    @my_posted_kata = MyPostedKata.find(params[:id])
   end
 
   # POST /my_posted_kata
   # POST /my_posted_kata.json
   def create
-    @my_posted_katum = MyPostedKatum.new(params[:my_posted_katum])
+    @my_posted_kata = MyPostedKata.new(params[:my_posted_kata])
 
     respond_to do |format|
-      if @my_posted_katum.save
-        format.html { redirect_to @my_posted_katum, notice: 'My posted katum was successfully created.' }
-        format.json { render json: @my_posted_katum, status: :created, location: @my_posted_katum }
+      if @my_posted_kata.save
+        format.html { redirect_to @my_posted_kata, notice: 'My posted katum was successfully created.' }
+        format.json { render json: @my_posted_kata, status: :created, location: @my_posted_kata }
       else
         format.html { render action: "new" }
-        format.json { render json: @my_posted_katum.errors, status: :unprocessable_entity }
+        format.json { render json: @my_posted_kata.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -56,15 +56,15 @@ class MyPostedKatasController < ApplicationController
   # PUT /my_posted_kata/1
   # PUT /my_posted_kata/1.json
   def update
-    @my_posted_katum = MyPostedKatum.find(params[:id])
+    @my_posted_kata = MyPostedKata.find(params[:id])
 
     respond_to do |format|
-      if @my_posted_katum.update_attributes(params[:my_posted_katum])
-        format.html { redirect_to @my_posted_katum, notice: 'My posted katum was successfully updated.' }
+      if @my_posted_kata.update_attributes(params[:my_posted_kata])
+        format.html { redirect_to @my_posted_kata, notice: 'My posted katum was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
-        format.json { render json: @my_posted_katum.errors, status: :unprocessable_entity }
+        format.json { render json: @my_posted_kata.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -72,8 +72,8 @@ class MyPostedKatasController < ApplicationController
   # DELETE /my_posted_kata/1
   # DELETE /my_posted_kata/1.json
   def destroy
-    @my_posted_katum = MyPostedKatum.find(params[:id])
-    @my_posted_katum.destroy
+    @my_posted_kata = MyPostedKata.find(params[:id])
+    @my_posted_kata.destroy
 
     respond_to do |format|
       format.html { redirect_to my_posted_kata_url }
