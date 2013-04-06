6e2507fe6a28cae569111dbc5efb519169548253
twer <twer@localhost.(none)>
2013-04-06 23:07:50 +0800

    jtong - finished all process

--- server/app/controllers/dev_exam_controller.rb
+++ server/app/controllers/dev_exam_controller.rb
@@ -17,8 +17,34 @@ class DevExamController < ApplicationController
     redirect_to :action => :kata
   end
 
+  def my_kata
+    @current_steps = 2
+    @kata = Kata.find params[:id]
+    @my_posted_kata = MyPostedKata.find_by_user_id current_user.id
+  end
+
+  def finish_kata
+    @current_steps = 2
+    @my_posted_kata = MyPostedKata.find params[:id]
+    @my_posted_kata.update_attributes params[:my_posted_kata]
+
+    redirect_to :action => :homework
+  end
+
   def homework
     @current_steps = 3
+    @my_posted_homework = MyPostedHomework.find_by_user_id current_user.id
+  end
+
+  def finish_homework
+    @current_steps = 3
+    @my_posted_homework = MyPostedHomework.find_by_user_id current_user.id
+    if !@my_posted_homework
+      @my_posted_homework = MyPostedHomework.new params[:my_posted_homework]
+      @my_posted_homework.user = current_user
+      @my_posted_homework.save!
+    end
+    redirect_to :action => :homework
   end
 
   def wizard
--- server/app/models/my_posted_homework.rb
+++ server/app/models/my_posted_homework.rb
@@ -1,5 +1,5 @@
 class MyPostedHomework < ActiveRecord::Base
   belongs_to :homework
-  belongs_to :user
-  attr_accessible :desc, :url
+  belongs_to :user, class_name: "MoodeUserPlugin::User"
+  attr_accessible :user, :homework, :desc, :url
 end
