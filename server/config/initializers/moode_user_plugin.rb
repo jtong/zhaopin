e35d35dbe85861918eecccaf7d0365ade5737ad4
twer <twer@localhost.(none)>
2013-04-10 10:09:50 +0800

    jtong - waiting 1 min for next sms

--- server/config/initializers/moode_user_plugin.rb
+++ server/config/initializers/moode_user_plugin.rb
@@ -5,5 +5,5 @@ MoodeUserPlugin.sms_server_config = {
   :service_prefix => "http://U.wangxun360.com",
   :send_uri => "/WS/Send.aspx"
 }
-MoodeUserPlugin.verify_code_message_template = "您的注册码是:{code}, 欢迎使用该注册码进行注册。"
+MoodeUserPlugin.verify_code_message_template = "This is your register code:{code}.We have to use English for a while,sorry."
       
